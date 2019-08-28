1abc4695661b4ab1121e54d939179bfc98e198b5
Savio <saviorabelo.ti@gmail.com>
2019-08-28 18:20:20 -0300

    Activation Function

--- Perceptron/Perceptron.py
+++ Perceptron/Perceptron.py
@@ -3,9 +3,11 @@ import numpy as np
 
 class Perceptron:
 
-    def __init__(self, x_data, y_data, realizations=10, learning_rate=0.05, epochs=1, outLayer=1, trainSize=0.8):
+    def __init__(self, x_data, y_data, activation='linear', realizations=1, \
+        learning_rate=0.05, epochs=200, outLayer=1, trainSize=0.8):
         self.x_data = x_data
         self.y_data = y_data
+        self.activation = activation
         self.attributes = x_data.shape[1]
         self.eta = learning_rate
         self.epochs = epochs
@@ -20,7 +22,7 @@ class Perceptron:
         self.accuracy = 0
         self.std = 0
 
-    def bias(self):
+    def insertBias(self):
         (m, _) = self.x_data.shape
         bias = -1 * np.ones((m,1))
         self.x_data = np.concatenate((bias, self.x_data), axis=1)
@@ -29,25 +31,19 @@ class Perceptron:
         # initializes weights randomly
         self.w = np.random.rand(self.outLayer, self.attributes+1)
     
-    def normalize(self):
+    def normalizeData(self):
         max_ = self.x_data.max(axis=0)
         min_ = self.x_data.min(axis=0)
         self.x_data = (self.x_data - min_) / (max_ - min_)
-
-    def degrau(self, u):
-        return np.array([1]) if u >= 0 else np.array([0])
-        #if u >= 0:
-        #    return np.array([1])
-        #return np.array([0])
     
-    def shuffle(self, x, y):
+    def shuffleData(self, x, y):
         data = list(zip(x, y))
         random.shuffle(data)
         x, y = zip(*data)
         return np.array(x), np.array(y)
     
-    def split(self):
-        self.x_data, self.y_data = self.shuffle(self.x_data, self.y_data)
+    def splitData(self):
+        self.x_data, self.y_data = self.shuffleData(self.x_data, self.y_data)
 
         (m, _) = self.x_data.shape
         x = int(m * self.trainSize) 
@@ -56,10 +52,21 @@ class Perceptron:
         self.x_test = self.x_data[x:]
         self.y_train = self.y_data[0:x]
         self.y_test = self.y_data[x:]
-    
+
+    def degrau(self, u):
+        return np.array([1]) if u >= 0 else np.array([0])
+
+    def activationFunction(self, u):
+        try:
+            if self.activation == 'linear':
+                return self.degrau(u)
+        except:
+            print('Error, activation function is not defined!')
+        
+
     def predict(self, xi):
         u = np.dot(self.w, xi)
-        y = self.degrau(u)
+        y = self.activationFunction(u)
         return y
 
     def train(self):
@@ -67,14 +74,15 @@ class Perceptron:
         cont_epochs = 0
         while (stop_error and cont_epochs < self.epochs):
             stop_error = 0
-            self.x_train, self.y_train = self.shuffle(self.x_train, self.y_train)
+            self.x_train, self.y_train = self.shuffleData(self.x_train, self.y_train)
             (m, _) = self.x_train.shape
             for i in range(m):
                 xi = self.x_train[i]
                 d = self.y_train[i]
 
                 u = np.dot(self.w, xi)
-                y = self.degrau(u)
+
+                y = self.activationFunction(u)
                 error = d - y
 
                 # check if this error
@@ -84,6 +92,7 @@ class Perceptron:
                 self.w += self.eta * (error * xi)
 
             cont_epochs += 1
+        #print('Number of epochs: {}'.format(cont_epochs), end='\n')
 
     def test(self):
         hits = 0.0
@@ -100,20 +109,21 @@ class Perceptron:
         
         self.hit_rate.append(hits/m)
 
-
     def perceptron(self):
-        self.initWeigths()
-        self.normalize()
-        self.bias()
+        self.normalizeData()
+        self.insertBias()
 
         for i in range(self.realizations):
-            self.split()
+            self.initWeigths()
+            self.splitData()
             self.train()
             self.test()
 
+        print('Hit rate: {}'.format(self.hit_rate))
         self.accuracy = np.mean(self.hit_rate)
-        print('Accuracy {:.3f}'.format(self.accuracy*100))
         self.std = np.std(self.hit_rate)
-        print('Std {:.3f}'.format(self.std))
+
+        print('Accuracy: {:.3f}'.format(self.accuracy*100))
+        print('Standard deviation: {:.3f}'.format(self.std))
 
 
--- Perceptron/main.py
+++ Perceptron/main.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from dataset import *
-import Perceptron as ps
+import Perceptron as PS
 
 def main():
     # import dataset
     attribute = 'Iris-setosa'
     x_data, y_data = irisFlowerBinary(attribute)
 
-    p = ps.Perceptron(x_data, y_data)
-    p.perceptron()
+    ps = PS.Perceptron(x_data, y_data)
+    ps.perceptron()
 
 if __name__ == '__main__':
     main()
